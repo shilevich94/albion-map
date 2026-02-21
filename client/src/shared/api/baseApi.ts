37254cabeae8ac40672462b7128edeabc98ec262
sergei-gilevich <kentserg9453@gmail.com>
2026-02-21 02:58:36 +0100

    Add render.yaml

--- client/src/shared/api/baseApi.ts
+++ client/src/shared/api/baseApi.ts
@@ -1,10 +1,14 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 
+const apiBaseUrl = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+  : '/api'
+
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/api',
+    baseUrl: apiBaseUrl,
   }),
   tagTypes: ['MarkedMap'],
   endpoints: () => ({}),
